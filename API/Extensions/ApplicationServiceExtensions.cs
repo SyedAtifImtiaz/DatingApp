0c7aface24e3f66fa2fb40692ac71f2687491d34
Syed Atif Imtiaz <atifimtiaz@gmail.com>
2024-01-29 07:51:56 +0500

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt => 
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        }); // initiate sqlite
+
+        services.AddCors(); // allow app to access
+        services.AddScoped<ITokenService, TokenService>(); // initialize token service 
+
+        return services;
+    }
+}
