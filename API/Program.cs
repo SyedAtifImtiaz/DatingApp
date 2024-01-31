cb16a38550b156da410bd1496289b9f29f163223
Syed Atif Imtiaz <atifimtiaz@gmail.com>
2024-01-31 15:23:16 +0500

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,5 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +12,13 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+// if(builder.Environment.IsDevelopment())
+// {
+//     app.UseDeveloperExceptionPage();
+// }
+
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
