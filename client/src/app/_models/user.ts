f6b86e6df582c43d3ceb85c10fbd8c2969c7f202
Syed Atif Imtiaz <atifimtiaz@gmail.com>
2024-02-05 19:09:15 +0500

    End of section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,4 +1,29 @@
-export interface User{
+export interface User {
     username: string;
     token: string;
 }
+
+// let data: number | string = 42;
+// data = "10";
+
+// //type script, making sure proper type value is used, required or not
+// interface Car{
+//     color: string;
+//     model: string;
+//     topSpeed?: number;
+// }
+
+// const car1: Car = {
+//     color: 'blue',
+//     model: 'BMW',
+// }
+
+// const car2: Car = {
+//     color: 'red',
+//     model: 'Mercedes',
+//     topSpeed: 100,
+// }
+
+// const multiply = (x: number, y:number) => {
+//     x * y;
+// }
